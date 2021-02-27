5c65007b36d7cd7459b57b2bb6492147fb935625
Wollgramm <bj.wollgramm@gmail.com>
2021-02-27 20:55:31 +0100

    commit

---
+++ EasyNetBug.DAL/Entities/Problem.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyNetBug.DAL.Entities
+{
+    public class Problem
+    {
+    }
+}
---
+++ EasyNetBug.GUI/Startup/BootStrapper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Prism.Unity;
+namespace EasyNetBug.GUI.Startup
+{
+
+    public class BootStrapper : Unity.Builder.
+    {
+    }
+}
