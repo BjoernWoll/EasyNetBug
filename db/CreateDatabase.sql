94b22262e115747104b17309ef4f3d19cd2871bc
Wollgramm <bj.wollgramm@gmail.com>
2021-03-09 17:03:41 +0100

    2.Commit

---
+++ db/CreateDatabase.sql
@@ -0,0 +1,115 @@
+﻿set xact_abort  on;
+begin transaction 
+drop table Content
+drop table IssueAssignPerson;
+drop table Issuetype;
+--issuemessage,IssueStatus,issue ;
+go
+
+create table severity
+(
+severity	nvarchar(10) primary key
+
+)
+
+go
+
+create table Product
+(
+id			int primary key identity,
+name		nvarchar(100),
+version		nvarchar(10)
+)
+go
+
+create table issue
+(
+id				int primary key identity(1,2),
+titel			nvarchar(20) not null,
+beschreibung	nvarchar(max),
+product			int not null,
+severity		nvarchar(10) not null default 'INFO',
+erstellt_am		datetime,
+update_am		datetime
+
+)
+go
+
+
+
+create table Issuetype
+(
+id				int primary key identity(1,2),
+Name			nvarchar(10) not null unique
+)
+go
+
+create table IssueIssuetype
+(
+id				int primary key identity(1,2),
+issue			int references issue(id),
+issuetype		int	references issuetype(id)
+
+)
+
+go
+
+
+create table Status
+(
+id				int primary key identity(1,2),
+Name			nvarchar(10) not null unique
+)
+
+go
+
+create table IssueStatus
+(
+id				int primary key identity(1,2),
+issue			int	references issue(id),
+status			int references status(id),
+message			nvarchar(100),
+changed_time	datetime
+)
+
+go
+
+create table Person
+(
+id				int primary key identity(1,2),
+Name			nvarchar(50) not null,			
+Vorname			nvarchar(50) not null,
+Mail			nvarchar(50) not null
+
+)
+go
+
+create table IssueAssignPerson
+(
+id				int primary key identity(1,2),
+person			int references person(id),
+issue			int references issue(id),
+assign_date		datetime
+
+)
+go
+
+
+create table Content
+(
+id				int primary key identity,
+text			nvarchar(max),
+url				nvarchar(50),
+file_content	varbinary(max),
+file_extension	nvarchar(10)
+)
+go
+create table issuemessage
+(
+id							int primary key identity(1,1),
+IssueAssignPerson			int references IssueAssignPerson(id),
+titel						nvarchar(50),
+Content						int references content(id),
+)
+go
+commit
---
+++ db/CreateExamples.sql
@@ -0,0 +1,28 @@
+﻿-- Generate Example Data
+
+insert into severity(severity) values('INFO');
+insert into severity(severity) values('BLOCKER');
+insert into severity(severity) values('BUG');
+insert into severity(severity) values('ERROR');
+
+--Personen
+insert into Person(Vorname,Name, mail) values('John', 'Doe','example@com.de');
+insert into Person(Vorname,Name, mail) values('John', 'Müller','J.Müller@ecom.de');
+
+insert into Product(name, version) values('DMS', '1.0');
+insert into Product(name, version) values('DMS', '2.0');
+insert into Product(name, version) values('Office', '1.0');
+
+insert into status(Name) values('OPEN');
+insert into status(Name) values('WORKING');
+insert into status(Name) values('CLOSED');
+insert into status(Name) values('REOPEN');
+
+insert into Issuetype(Name) values('BUG');
+insert into Issuetype(Name) values('REQUEST');
+
+
+go
+
+insert into issue(titel,beschreibung,severity,erstellt_am,product)
+			values('Test Bug im DMS', 'Bla bla blub', 3, Getdate(),1);
